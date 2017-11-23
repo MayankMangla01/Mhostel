51dce53eb47e631fe4cdc08b09e6b84d8dc5f9d1
Unknown <areshhitman@gmail.com>
2017-11-24 02:24:15 +0530

    database linked with login activity and registration. Also, forget password is working.
    Please use it once and see

--- app/src/main/java/com/example/vampcat/hostelm/LoginPage.java
+++ app/src/main/java/com/example/vampcat/hostelm/LoginPage.java
@@ -1,75 +1,146 @@
 package com.example.vampcat.hostelm;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.widget.CircularProgressDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.client.Firebase;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginPage extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    TextView fp ;
+    EditText userName ;
+    EditText password ;
+    String sUserName ;
+    String sPassword ;
+    ProgressDialog progressDialog;
+    Boolean EditTextEmptyCheck;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
         mAuth = FirebaseAuth.getInstance();
+        fp = (TextView) findViewById(R.id.TVforgotpassword);
+        userName = (EditText) findViewById(R.id.ETusername);
+        password = (EditText) findViewById(R.id.ETpassword);
+        fp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String resetemail = userName.getText().toString().trim();
+                //    progressDialog.setMessage("Please Wait");
+
+                // Showing progressDialog.
+                //    progressDialog.show();
+                mAuth.sendPasswordResetEmail(resetemail)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    //    progressDialog.dismiss();
+
+                                    Toast.makeText(LoginPage.this, "Reset password mail sent", Toast.LENGTH_LONG).show();
+                                    password.setText("");
+                                }
+                            }
+                        });
+            }
+        });
+    }
+    // Creating method to check EditText is empty or not.
+    public void CheckEditTextIsEmptyOrNot(){
+
+        Log.d("test","verify");
+
+        // Getting value form Email's EditText and fill into EmailHolder string variable.
+        sUserName = userName.getText().toString().trim();
+
+        // Getting value form Password's EditText and fill into PasswordHolder string variable.
+        sPassword = password.getText().toString().trim();
+
+        // Checking Both EditText is empty or not.
+        if(TextUtils.isEmpty(sUserName) || TextUtils.isEmpty(sPassword))
+        {
+
+            // If any of EditText is empty then set value as false.
+            EditTextEmptyCheck = false;
+
+        }
+        else {
+
+            // If any of EditText is empty then set value as true.
+            EditTextEmptyCheck = true ;
+
+        }
+
     }
 
-    public void onLogin(View v)
-    {
-        if(v.getId() == R.id.Blogin)
-        {
-            EditText userName = (EditText)findViewById(R.id.ETusername);
-            EditText password = (EditText)findViewById(R.id.ETpassword);
-            String sUserName = userName.getText().toString();
-            String sPassword = password.getText().toString();
-            Intent i = new Intent(LoginPage.this,SuccessfulLogin.class );
-            i.putExtra("username",sUserName);
-            startActivity(i);
 
-            if(TextUtils.isEmpty(sUserName) || TextUtils.isEmpty(sPassword))
+    public void onLogin(View v) {
+        if (v.getId() == R.id.Blogin) {
 
-                mAuth.createUserWithEmailAndPassword(sUserName, sPassword)
+            sUserName = userName.getText().toString().trim();
+            sPassword = password.getText().toString().trim();
+            CheckEditTextIsEmptyOrNot();
+            if(EditTextEmptyCheck) {
+
+                mAuth.signInWithEmailAndPassword(sUserName, sPassword)
                         .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-//                            if (task.isSuccessful()) {
-//                                // Sign in success, update UI with the signed-in user's information
-//                                Log.d(TAG, "createUse rWithEmail:success");
-//                                FirebaseUser user = mAuth.getCurrentUser();
-//                                updateUI(user);
-//                            } else {
-//                                // If sign in fails, display a message to the user.
-//                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
-//                                Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-//                                        Toast.LENGTH_SHORT).show();
-//                                updateUI(null);
-//                            }
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("email", "createUse rWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Intent i = new Intent(LoginPage.this, SuccessfulLogin.class);
+                                    i.putExtra("username", sUserName);
+                                    startActivity(i);
+                                    //updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("email", "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(LoginPage.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    // updateUI(null);
+                                }
 
                                 // ...
                             }
                         });
+            }
+            else{
+
+                // If  EditTextEmptyCheck == false then toast display on screen.
+                Toast.makeText(LoginPage.this, "Please Fill All the Fields", Toast.LENGTH_LONG).show();
+            }
+
+            //if(v.getId() == R.id.TVforgotpassword)
+
+
+            //Intent i =  new Intent(this, ForgotPassword.class);
+
+            // startActivity(i);
 
         }
-        if(v.getId() == R.id.TVforgotpassword)
-        {
-            Intent i =  new Intent(this, ForgotPassword.class);
-            startActivity(i);
-
-        }
-        if(v.getId() == R.id.TVregister)
-        {
-            Intent i =  new Intent(this, Registration.class);
+        if (v.getId() == R.id.TVregister) {
+            Intent i = new Intent(this, Registration.class);
             startActivity(i);
         }
     }
+
 }
--- app/src/main/java/com/example/vampcat/hostelm/Registration.java
+++ app/src/main/java/com/example/vampcat/hostelm/Registration.java
@@ -1,60 +1,184 @@
 package com.example.vampcat.hostelm;
 
-import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.text.TextUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.firebase.client.Firebase;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-/**
- * Created by Vampcat on 17 November.
- */
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Registration extends Activity {
-    private Firebase mRootRef;
+public class Registration extends AppCompatActivity {
 
+    private static EditText fullName, emailId, mobileNumber, rollno,
+            password, confirmPassword;
+    private static TextView login;
+    private static Button signUpButton;
+    private static CheckBox terms_conditions;
+    private DatabaseReference mDatabase;
+    //  private Firebase Emp_reg;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    ProgressDialog progressDialog;
+    // Initialize all views
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration);
-        mRootRef = new Firebase("https://hostel-m-5fc38.firebaseio.com/Users")
-    }
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference Emp_reg = database.getReference("Emp_Registration");
 
-    public void onRegister(View v)
-    {
-        if(v.getId() == R.id.Rregister)
-        {
-            EditText name = (EditText)findViewById(R.id.Rname);
-            EditText rollno = (EditText)findViewById(R.id.Rrollnumber);
-            EditText email = (EditText)findViewById(R.id.Remail);
-            EditText pass = (EditText)findViewById(R.id.Rpassword);
-            EditText cpass = (EditText)findViewById(R.id.Rconfirmpassword);
+        //Emp_reg = new Firebase("https://employee-tracking-system-7df39.firebaseio.com/Emp_Registration");
 
-            String sname = name.getText().toString();
-            String srollno = rollno.getText().toString();
-            String semail = email.getText().toString();
-            String spass = pass.getText().toString();
-            String scpass = cpass.getText().toString();
+        fullName = (EditText) findViewById(R.id.fullName);
+        emailId = (EditText) findViewById(R.id.userEmailId);
+        mobileNumber = (EditText) findViewById(R.id.mobileNumber);
+        rollno = (EditText) findViewById(R.id.location);
+        password = (EditText) findViewById(R.id.password);
+        confirmPassword = (EditText) findViewById(R.id.confirmPassword);
+        signUpButton = (Button) findViewById(R.id.signUpBtn);
 
-            if(!spass.equals(scpass))
-            {
-                //pop up msg
-                Toast tpass = Toast.makeText(this, "Passwords do not match!", Toast.LENGTH_SHORT);
-                tpass.show();
+        terms_conditions = (CheckBox) findViewById(R.id.terms_conditions);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+
+
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (checkValidation()) {
+                    String name = fullName.getText().toString().trim();
+                    String email = emailId.getText().toString().trim();
+                    String mobileno = mobileNumber.getText().toString().trim();
+                    String loc = rollno.getText().toString().trim();
+                    String pass = password.getText().toString().trim();
+                    String des = "Employee";
+                    String task = "Null";
+
+                    mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(Registration.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Log.v("status", "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                            // If sign in fails, display a message to the user. If sign in succeeds
+                            // the auth state listener will be notified and logic to handle the
+                            // signed in user can be handled in the listener.
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(Registration.this, "Password Short", Toast.LENGTH_SHORT).show();
+                                password.setText("");
+                                confirmPassword.setText("");
+                            } else if (task.isSuccessful()) {
+                                Toast.makeText(Registration.this, "Successful", Toast.LENGTH_SHORT).show();
+                                fullName.setText("");
+                                emailId.setText("");
+                                mobileNumber.setText("");
+                                rollno.setText("");
+                                password.setText("");
+                                confirmPassword.setText("");
+                            }
+
+                            // ...
+                        }
+                    });
+
+                    String key = Emp_reg.push().getKey();
+                    Emp_details emp = new Emp_details(key, name, email, mobileno, loc, des, task);
+                    Map<String, Object> postValues = emp.toMap();
+
+                    Map<String, Object> childUpdates = new HashMap<>();
+                    childUpdates.put("/Emp_details/" + key, postValues);
+
+                    mDatabase.updateChildren(childUpdates);
+                }
             }
+        });
+        // Setting text selector over textviews
+        // XmlResourceParser xrp = getResources().getXml(R.drawable.text_selector);
+        //try {
+        //    ColorStateList csl = ColorStateList.createFromXml(getResources(),
+        //            xrp);
 
-            Firebase childRef = mRootRef.child("xName");
-            childRef.setValue(sname);
-
-        }
+        //   login.setTextColor(csl);
+        //   terms_conditions.setTextColor(csl);
+        // } catch (Exception e) {
+        // }
     }
 
-}
+    // Check Validation Method
+    private boolean checkValidation() {
+        // Get all edittext texts
+        String getFullName = fullName.getText().toString();
+        String getEmailId = emailId.getText().toString();
+        String getMobileNumber = mobileNumber.getText().toString();
+        String getLocation = rollno.getText().toString();
+        String getPassword = password.getText().toString();
+        String getConfirmPassword = confirmPassword.getText().toString();
 
+        // Pattern match for email id
+        Pattern p = Pattern.compile(".+@thapar\\.edu");
+        Matcher m = p.matcher(getEmailId);
+
+        // Check if all strings are null or not
+        if (getFullName.equals("") || getFullName.length() == 0
+                || getEmailId.equals("") || getEmailId.length() == 0
+                || getMobileNumber.equals("") || getMobileNumber.length() == 0
+                || getLocation.equals("") || getLocation.length() == 0
+                || getPassword.equals("") || getPassword.length() == 0
+                || getConfirmPassword.equals("")
+                || getConfirmPassword.length() == 0) {
+
+            // new CustomToast().Show_Toast(this, view,
+            //        "All fields are required.");
+            Toast.makeText(getApplicationContext(), "All fields are required", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        // Check if email id valid or not
+        else if (!m.find()) {
+//            new CustomToast().Show_Toast(this, view,
+//                    "Your Email Id is Invalid.");
+
+            Toast.makeText(getApplicationContext(), "Your Email Id is Invalid.", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        // Check if both password should be equal
+        else if (!getConfirmPassword.equals(getPassword)) {
+//            new CustomToast().Show_Toast(this, view,
+//                    "Both password doesn't match.");
+            Toast.makeText(getApplicationContext(), "Both password doesn't match.", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+
+        // Make sure user should check Terms and Conditions checkbox
+        else if (!terms_conditions.isChecked()) {
+//            new CustomToast().Show_Toast(this, view,
+//                    "Please select Terms and Conditions.");
+            Toast.makeText(getApplicationContext(), "Please select Terms and Conditions.", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        return true;
+
+
+    }}
